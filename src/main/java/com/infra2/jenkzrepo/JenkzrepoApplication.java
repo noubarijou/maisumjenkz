fb14175d844a7b6a4e6df6b0973f0dee538429fe
noubarijou <noubarijou@live.com>
2021-11-23 08:36:30 -0300

    my first commit

---
+++ src/main/java/com/infra2/jenkzrepo/JenkzrepoApplication.java
@@ -0,0 +1,13 @@
+package com.infra2.jenkzrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkzrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkzrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infra2/jenkzrepo/JenkzrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infra2.jenkzrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkzrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
